4f5047f709adcb8e657058a7e0a9d4c47687f43b
sreshetnyak <reshetniak.sergey@gmail.com>
2014-02-14 10:49:57 +0200

    Initial commit from local git

---
+++ UIViewAnimation/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  UIViewAnimation
+//
+//  Created by sergey on 2/14/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
